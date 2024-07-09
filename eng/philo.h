a32107e10c8b35ac9ad50a6a266c8194627ffc64
Said Bora Ozdamar <sozdamar@k1m42s05.42istanbul.com.tr>
2024-07-09 13:44:01 +0300

    new ver

---
+++ eng/philo.h
@@ -0,0 +1,54 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+# define PHILO_MAX 300
+
+typedef struct s_philo
+{
+	pthread_t	thread;
+	int			id;
+	int			eating;
+	int			eaten_meals;
+	int			num_philos;
+	int			num_eat;
+	int			*dead;
+	size_t		last_meal;
+	size_t		time_to_die;
+	size_t		time_to_eat;
+	size_t		time_to_sleep;
+	size_t		start_time;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*write_lock;
+	pthread_mutex_t	*dead_lock;
+	pthread_mutex_t	*meal_lock;
+}	t_philo;
+
+typedef struct s_program
+{
+	int				dead_flag;
+	pthread_mutex_t	write_lock;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	meal_lock;
+	t_philo			*philos;
+}	t_program;
+
+//main
+
+//init
+
+//threads
+
+//actions
+
+//monitor
+
+//utils
+
+# endif
